01981ad043a1a1631cf1bf91bb561a049a660d2f
zagariy_ds <zagariy_ds@NL-L-7K311B3>
2022-01-19 11:33:03 +0300

    Добавьте файлы проекта.

---
+++ Empty/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Empty.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // Контроллер должен быть унаследован от класса Controller и в данном случае он имеет один метод Index, который с помощью метода View обращается к представлению.
+        }
+    }
+}
---
+++ Empty/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace Empty
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();  // с помощью вызова services.AddMvc() добавляются все сервисы MVC. После этого мы можем использовать фреймворк.
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting(); // В методе Configure() через вызовы app.UseRouting() и app.UseEndpoints() встраивается система маршрутизации, которая позволяет связать приходящие от пользователей запросы с контроллерами.
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
